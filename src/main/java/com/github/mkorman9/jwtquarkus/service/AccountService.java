d4c1eb6e443f137be19f91bffe5dfafc54998540
Michal Korman <michal.korman@icloud.com>
2023-06-21 19:04:36 +0200

    add log to AccountService

--- src/main/java/com/github/mkorman9/jwtquarkus/service/AccountService.java
+++ src/main/java/com/github/mkorman9/jwtquarkus/service/AccountService.java
@@ -1,12 +1,14 @@
 package com.github.mkorman9.jwtquarkus.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @ApplicationScoped
+@Slf4j
 public class AccountService {
     private final Map<UUID, Boolean> ACCOUNTS = new ConcurrentHashMap<>();
     private final Map<Long, UUID> GITHUB_CONNECTIONS = new ConcurrentHashMap<>();
@@ -14,11 +16,10 @@ public class AccountService {
     public UUID registerAccount() {
         var account = UUID.randomUUID();
         ACCOUNTS.put(account, true);
-        return account;
-    }
 
-    public boolean doesAccountExist(UUID id) {
-        return ACCOUNTS.containsKey(id);
+        log.info("Registered new account {}", account);
+
+        return account;
     }
 
     public void connectAccount(long githubId, UUID id) {
