aa16b044f641be4c86f8b0ad34477b94df1c1f3f
Michal Korman <michal.korman@icloud.com>
2023-09-26 00:44:26 +0200

    add comments to AuditEventsListener

--- src/main/java/com/github/mkorman9/jwtquarkus/audit/AuditEventsListener.java
+++ src/main/java/com/github/mkorman9/jwtquarkus/audit/AuditEventsListener.java
@@ -7,7 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 @ApplicationScoped
 @Slf4j
 public class AuditEventsListener {
-    @ConsumeEvent(value = AuditEvent.CHANNEL_ADDRESS, blocking = true)
+    @ConsumeEvent(value = AuditEvent.CHANNEL_ADDRESS)
+    // @Blocking to use worker thread
+    // @RunOnVirtualThread to use virtual thread
     public void onAuditEvent(AuditEvent auditEvent) {
         log.info("[AUDIT] {} {}", auditEvent.action(), auditEvent.subject());
     }
