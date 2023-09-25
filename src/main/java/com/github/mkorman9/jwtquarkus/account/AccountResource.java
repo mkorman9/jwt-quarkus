669e93a1d4894e5199d64f1c164b25b18d68b80c
Michal Korman <michal.korman@icloud.com>
2023-09-25 22:15:55 +0200

    avoid start imports

--- src/main/java/com/github/mkorman9/jwtquarkus/account/AccountResource.java
+++ src/main/java/com/github/mkorman9/jwtquarkus/account/AccountResource.java
@@ -1,15 +1,20 @@
 package com.github.mkorman9.jwtquarkus.account;
 
+import com.github.mkorman9.jwtquarkus.token.TokenFacade;
 import com.github.mkorman9.jwtquarkus.token.TokenPair;
 import com.github.mkorman9.jwtquarkus.token.TokenResponse;
 import com.github.mkorman9.jwtquarkus.token.exception.TokenRefreshException;
 import com.github.mkorman9.jwtquarkus.token.exception.TokenValidationException;
-import com.github.mkorman9.jwtquarkus.token.TokenFacade;
 import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import lombok.Builder;
