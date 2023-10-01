0ef48e87f66ad9873c5bac9bc0029dc4f0ea0cba
Michal Korman <michal.korman@icloud.com>
2023-10-01 21:16:42 +0200

    add tests for /oauth/connect-account

--- src/test/java/com/github/mkorman9/jwtquarkus/oauth/OauthResourceTest.java
+++ src/test/java/com/github/mkorman9/jwtquarkus/oauth/OauthResourceTest.java
@@ -1,5 +1,6 @@
 package com.github.mkorman9.jwtquarkus.oauth;
 
+import com.github.mkorman9.jwtquarkus.account.AccountResource;
 import com.github.mkorman9.jwtquarkus.oauth.github.GithubAPI;
 import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
@@ -41,5 +42,45 @@ class OauthResourceTest {
 
         var isValid = oauthStateService.validateState(state, cookie).valid();
         assertThat(isValid).isTrue();
+    }
+
+    @Test
+    public void testConnectAccount() {
+        Mockito.when(githubAPI.getConnectAccountUrl(Mockito.anyString())).thenAnswer(invocation -> {
+            var state = (String) invocation.getArgument(0);
+            return URI.create("https://example.com/?state=" + state);
+        });
+
+        var newAccountResponse = given()
+            .when().get("/account/new")
+            .then()
+            .statusCode(200)
+            .extract().body().as(AccountResource.AccountResponse.class);
+        var redirectResponse = given()
+            .redirects().follow(false)
+            .when()
+            .queryParam("accessToken", newAccountResponse.token().accessToken())
+            .get("/oauth/connect-account")
+            .then()
+            .statusCode(303);
+
+        var location = URI.create(redirectResponse.extract().header("Location"));
+        var cookie = redirectResponse.extract().cookie("oauth2_cookie");
+        var state = URLEncodedUtils.parse(location, Charset.defaultCharset()).get(0).getValue();
+
+        var stateValidation = oauthStateService.validateState(state, cookie);
+        assertThat(stateValidation.valid()).isTrue();
+        assertThat(stateValidation.subject()).isEqualTo(newAccountResponse.id());
+    }
+
+    @Test
+    public void testConnectAccountInvalidToken() {
+        given()
+            .redirects().follow(false)
+            .when()
+            .queryParam("accessToken", "INVALID_TOKEN")
+            .get("/oauth/connect-account")
+            .then()
+            .statusCode(401);
     }
 }
